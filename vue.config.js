bc0d99c63dfb401989a884e99a16e42bc1fa5cf6
tingrui798 <1835879427@qq.com>
2019-10-10 12:16:09 +0800

    初始化项目

---
+++ vue.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+
+    configureWebpack: {
+  
+  ​    resolve: {
+  
+  ​      alias: {
+  
+  ​        'components': '@/components',
+  
+  ​        'content': 'components/content',
+  
+  ​        'common': 'components/common',
+  
+  ​        'assets': '@/assets',
+  
+  ​        'network': '@/network',
+  
+  ​        'views': '@/views',
+  
+  ​      }
+  
+  ​    }
+  
+    }
+  
+  }
